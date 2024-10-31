7185dac4cbc25abd62231b6f6eb7486ed728cb11
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-10-31 02:38:16 -0500

    🧟 Zombie horde

--- challenges/challenge02.js
+++ challenges/challenge02.js
@@ -2,8 +2,8 @@ function battleHorde(zombies, humans) {
   let power = 0
 
   for (let [key, value] of [...zombies].entries()) {
-    power += +value - humans[key]
+    power += value - humans[key]
   }
 
-  return power ? `${Math.abs(power)}${'hz'.at(power > 0)}` : 'x'
+  return `${Math.abs(power) || ''}${'xzh'.at(Math.sign(power))}`
 }
