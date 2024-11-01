b1f582e333bcc0b2b60e1ca26d95313b41f18f7a
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-11-01 02:37:56 -0500

    🛌 Escape from Freddy's nightmare

--- challenges/challenge03.js
+++ challenges/challenge03.js
@@ -1,21 +1,11 @@
 function findSafestPath(dream) {
-  const n = dream.length
-  const m = dream[0].length
-
-  const dp = new Array(m).fill(0)
-  dp[0] = dream[0][0]
-
-  for (let i = 1; i < m; i++) {
-    dp[i] = dp[i - 1] + dream[0][i]
-  }
-
-  for (let i = 1; i < n; i++) {
-    dp[0] += dream[i][0]
-
-    for (let j = 1; j < m; j++) {
-      dp[j] = dream[i][j] + Math.min(dp[j], dp[j - 1])
+  let i = 0
+  for (const row of dream) {
+    row[0] += dream[i - 1]?.[0] | 0
+    for(let j = 1; j < row.length; j++) {
+      row[j] += Math.min(row[j - 1], dream[i - 1]?.[j] || Infinity)
     }
+    i++
   }
-
-  return dp[m - 1]
+  return dream[i - 1][dream[0].length - 1]
 }
