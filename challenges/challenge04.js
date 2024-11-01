b7ea0ce67f434aa36662d60b21804c3699886a3b
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-11-01 03:13:54 -0500

    🔪 Find the killer

--- challenges/challenge04.js
+++ challenges/challenge04.js
@@ -1,4 +1,4 @@
 function findTheKiller(whisper, suspects) {
-  const re = new RegExp(`^${whisper.replace(/~/g, '.')}`, 'i')
-  return suspects.filter(suspect => re.test(suspect)).join(',')
+  const regex = new RegExp(`^${whisper.replace(/~/g, '.')}`, 'i')
+  return suspects.filter(suspect => regex.test(suspect)).join(',')
 }
